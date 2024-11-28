209d2e88c94988f0b8c30c572926a53b82c3d6c9
blupants <blupants.robot@gmail.com>
2024-11-28 17:36:01 -0600

    Asana mapping

--- src/n0s1/controllers/asana_controller.py
+++ src/n0s1/controllers/asana_controller.py
@@ -52,47 +52,46 @@ class AsanaController(hollow_controller.HollowController):
         return False
 
     def _get_workspaces(self, limit=None):
+        workspaces = []
         if self._scan_scope:
-            workspaces = []
-            for key in self._scan_scope:
-                workspaces.append(key)
+            for key in self._scan_scope.get("workspaces", {}):
+                w = self._client.workspaces.get_workspace(key)
+                workspaces.append(w)
+        if len(workspaces) > 0:
             return workspaces
         self.connect()
         return self._client.workspaces.get_workspaces()
 
     def _get_projects(self, workspace_gid, limit=None):
+        projects = []
         if self._scan_scope:
-            workspaces = self._get_workspaces(limit)
-            for w in workspaces:
-                if workspace_gid == w.get("gid", ""):
-                    return w.get("projects", {})
+            for key in self._scan_scope.get("workspaces", {}).get(workspace_gid, {}).get("projects", {}):
+                p = self._client.projects.get_project(key)
+                projects.append(p)
+        if len(projects) > 0:
+            return projects
         self.connect()
         return self._client.projects.get_projects_for_workspace(workspace_gid)
 
     def _get_tasks(self, workspace_gid, project_gid, limit=None):
+        tasks = []
         if self._scan_scope:
-            workspaces = self._get_workspaces(limit)
-            for w in workspaces:
-                if workspace_gid == w.get("gid", ""):
-                    projects = self._get_projects(workspace_gid, limit)
-                    for p in projects:
-                        if projects == p.get("gid", ""):
-                            return p.get("tasks", {})
+            for key in self._scan_scope.get("workspaces", {}).get(workspace_gid, {}).get("projects", {}).get(project_gid, {}).get("tasks", {}):
+                t = self._client.tasks.get_task(key, opt_fields=["name", "gid", "notes", "permalink_url"])
+                tasks.append(t)
+        if len(tasks) > 0:
+            return tasks
         self.connect()
         return self._client.tasks.get_tasks_for_project(project_gid, opt_fields=["name", "gid", "notes", "permalink_url"])
 
     def _get_stories(self, workspace_gid, project_gid, task_gid, limit=None):
+        stories = []
         if self._scan_scope:
-            workspaces = self._get_workspaces(limit)
-            for w in workspaces:
-                if workspace_gid == w.get("gid", ""):
-                    projects = self._get_projects(workspace_gid, limit)
-                    for p in projects:
-                        if projects == p.get("gid", ""):
-                            tasks = self._get_tasks(workspace_gid, project_gid, limit=None)
-                            for t in tasks:
-                                if task_gid == t.get("gid", ""):
-                                    return t.get("stories", {})
+            for key in self._scan_scope.get("workspaces", {}).get(workspace_gid, {}).get("projects", {}).get(project_gid, {}).get("tasks", {}).get(task_gid, {}).get("stories", {}):
+                s = self._client.stories.get_story(key)
+                stories.append(s)
+        if stories:
+            return stories
         self.connect()
         return self._client.stories.get_stories_for_task(task_gid)
 
@@ -104,25 +103,47 @@ class AsanaController(hollow_controller.HollowController):
             for w in workspaces:
                 workspace_gid = w.get("gid", "")
                 if len(workspace_gid) > 0:
-                    w["projects"] = {}
-                    map_data["workspaces"][workspace_gid] = w
+                    w_item = {
+                        "gid": workspace_gid,
+                        "name": w.get("name", ""),
+                        "projects": {}
+                    }
+                    map_data["workspaces"][workspace_gid] = w_item
+                    if levels > 0 and levels <= 1:
+                        continue
                     if projects := self._get_projects(workspace_gid, limit):
                         for p in projects:
-                            p["tasks"] = {}
                             project_gid = p.get("gid", "")
+                            p_item = {
+                                "gid": project_gid,
+                                "name": p.get("name", ""),
+                                "tasks": {}
+                            }
                             if len(project_gid) > 0:
-                                map_data["workspaces"][workspace_gid]["projects"][project_gid] = p
+                                map_data["workspaces"][workspace_gid]["projects"][project_gid] = p_item
+                            if levels > 0 and levels <= 2:
+                                continue
                             tasks = self._get_tasks(workspace_gid, project_gid)
                             for t in tasks:
-                                t["stories"] = {}
                                 task_gid = t.get("gid", "")
+                                t_item = {
+                                    "gid": task_gid,
+                                    "name": t.get("name", ""),
+                                    "stories": {}
+                                }
                                 if len(project_gid) > 0:
-                                    map_data["workspaces"][workspace_gid]["projects"][project_gid]["tasks"][task_gid] = t
+                                    map_data["workspaces"][workspace_gid]["projects"][project_gid]["tasks"][task_gid] = t_item
+                                if levels > 0 and levels <= 3:
+                                    continue
                                 stories = self._get_stories(workspace_gid, project_gid, task_gid, limit)
                                 for s in stories:
                                     story_gid = s.get("gid", "")
+                                    s_item = {
+                                        "gid": story_gid,
+                                        "name": s.get("name", "")
+                                    }
                                     if len(story_gid) > 0:
-                                        map_data["workspaces"][workspace_gid]["projects"][project_gid]["tasks"][task_gid]["stories"][story_gid] = s
+                                        map_data["workspaces"][workspace_gid]["projects"][project_gid]["tasks"][task_gid]["stories"][story_gid] = s_item
 
         return map_data
 
