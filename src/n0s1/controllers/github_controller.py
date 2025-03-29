0fbed6555f6c7dad6140fb2681700cba711fe180
blupants <blupants.robot@gmail.com>
2025-03-28 23:17:05 -0500

    Added support to scan for leaked secrets in GitHub. Added mapping

--- src/n0s1/controllers/github_controller.py
+++ src/n0s1/controllers/github_controller.py
@@ -33,45 +33,152 @@ class GitHubController(hollow_controller.HollowController):
                 return False
         return False
 
-    def get_data(self, include_comments=False, limit=None):
-        if not self._client:
-            return {}
+    def _get_repo_fullname(self, repo_name):
+        owner = self._owner
+        if not owner or len(owner) <= 0:
+            owner = self._client.get_user().login
+        repo_fullname = owner + "/" + repo_name
+        return repo_fullname
 
-        repos = None
+    def _get_repo_obj(self, repo_gid=None):
+        if self._repo and len(self._repo) > 0:
+            repo_name = self._get_repo_fullname(self._repo)
+            return self._client.get_repo(repo_name)
+        full_name = repo_gid.lower().replace("git://github.com/", "")
+        full_name = full_name[:-4]
+        return self._client.get_repo(full_name)
+
+    def _get_repos(self):
+        repos = []
+
+        if self._repo and len(self._repo) > 0:
+            return [self._get_repo_obj()]
+
+        if self._scan_scope and self._owner and len(self._owner):
+            for key in self._scan_scope.get("repos", {}).get(self._owner, {}).get("repos", {}):
+                p = self._client.get_repo(key)
+                repos.append(p)
+        if len(repos) > 0:
+            return repos
+        self.connect()
         if self._owner and len(self._owner) > 0:
             org = self._client.get_organization(self._owner)
             repos = org.get_repos()
         else:
             repos = self._client.get_user().get_repos()
+        return repos
 
-        for repo in repos:
-            print(f"Searching in repository: {repo.name}")
+    def _get_branches(self, repo_gid, limit=None):
+        branches = []
+        if self._scan_scope:
+            for key in self._scan_scope.get("repos", {}).get(repo_gid, {}).get("branches", {}):
+                b = self._client.projects.get_project(key)
+                branches.append(b)
+        if len(branches) > 0:
+            return branches
+        self.connect()
+        repo_obj = self._get_repo_obj(repo_gid)
+        if repo_obj:
+            branches = repo_obj.get_branches()
+        return branches
 
-            # Iterate through each branch
-            for branch in repo.get_branches():
-                print(f"  Checking branch: {branch.name}")
+    def _get_files(self, repo_gid, branch_gid, limit=None):
+        files = []
+        self.connect()
+        repo_obj = self._get_repo_obj(repo_gid)
+        if self._scan_scope:
+            for key in self._scan_scope.get("repos", {}).get(repo_gid, {}).get("branches", {}).get(branch_gid, {}).get("files", {}):
+                f = repo_obj.get_contents(key, ref=branch_gid)
+                files.append(f)
+        if len(files) > 0:
+            return files
+        if repo_obj:
+            files = []
+            try:
+                contents = repo_obj.get_contents("", ref=branch_gid)
+                while contents:
+                    file_content = contents.pop(0)
+                    if file_content.type == "dir":
+                        contents.extend(repo_obj.get_contents(file_content.path, ref=branch_gid))
+                    else:
+                        files.append(file_content.path)
+            except Exception as e:
+                message = f"Error listing files from branch {branch_gid}: {e}"
+                self.log_message(message, logging.ERROR)
+        return files
 
-                # Iterate through each file in the branch
-                try:
-                    contents = repo.get_contents("", ref=branch.name)
-                    while contents:
-                        file_content = contents.pop(0)
-                        if file_content.type == "dir":
-                            contents.extend(repo.get_contents(file_content.path, ref=branch.name))
-                        else:
-                            # Fetch file content and search for the term
-                            file_data = repo.get_contents(file_content.path,
-                                                          ref=branch.name).decoded_content.decode(errors='ignore')
-                            url = repo.html_url + f"/blob/{branch.name}/{file_content.path}"
+    def get_mapping(self, levels=-1, limit=None):
+        if not self._client:
+            return {}
+        map_data = {"repos": {}}
+        if repos := self._get_repos():
+            for repo in repos:
+                repo_gid = repo.git_url
+                message = f"Searching in repository: {repo.html_url}"
+                self.log_message(message, logging.INFO)
+                if len(repo_gid) > 0:
+                    r_item = {
+                        "gid": repo_gid,
+                        "name": repo.name,
+                        "branches": {}
+                    }
+                    map_data["repos"][repo_gid] = r_item
+                    if levels > 0 and levels <= 1:
+                        continue
+                    if branches := self._get_branches(repo_gid, limit):
+                        for branch in branches:
+                            message = f"Searching in branch: {branch.name}"
+                            self.log_message(message, logging.INFO)
+                            branch_gid = branch.name
+                            b_item = {
+                                "gid": branch.commit.sha,
+                                "name": branch.name,
+                                "files": {}
+                            }
+                            if len(branch_gid) > 0:
+                                map_data["repos"][repo_gid]["branches"][branch_gid] = b_item
+                            if levels > 0 and levels <= 2:
+                                continue
+                            files = self._get_files(repo_gid, branch_gid)
+                            map_data["repos"][repo_gid]["branches"][branch_gid]["files"] = files
+                            if levels > 0 and levels <= 3:
+                                continue
+        return map_data
+
+    def get_data(self, include_comments=False, limit=None):
+        if not self._client:
+            return {}
+
+        if repos := self._get_repos():
+            for repo in repos:
+                message = f"Searching in repository: {repo.html_url}"
+                self.log_message(message, logging.INFO)
+
+                # Iterate through each branch
+                repo_gid = repo.full_name
+                for branch in self._get_branches(repo_gid):
+                    message = f"Searching in branch: {branch.name}"
+                    self.log_message(message, logging.INFO)
+
+                    # Iterate through each file in the branch
+                    try:
+                        files = self._get_files(repo_gid, branch.name)
+                        for f in files:
+                            # Fetch file content
+                            file_data = repo.get_contents(f, ref=branch.name).decoded_content.decode(errors='ignore')
+                            url = repo.html_url + f"/blob/{branch.name}/{f}"
                             file = self.pack_data(file_data, url)
                             yield file
-                except Exception as e:
-                    print(f"  Error accessing branch {branch.name}: {e}")
+                    except Exception as e:
+                        message = f"Error accessing branch {branch.name}: {e}"
+                        self.log_message(message, logging.ERROR)
 
     def post_comment(self, issue, comment):
         if not self._client:
             return False
-        return self._client.post_comment(issue, comment)
+        message = f"Unable to post comment to {issue}!"
+        self.log_message(message, logging.ERROR)
+        return False
 
     def pack_data(self, file_data, url):
         ticket_data = {
